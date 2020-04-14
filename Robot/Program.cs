412c41f5ac8a59bc8a7458ff97fea3b1a062b8b1
James Furey <jfurey@mywebgrocer.com>
2020-04-14 22:57:48 +0100

    readline

--- Robot/Program.cs
+++ Robot/Program.cs
@@ -14,7 +14,8 @@ namespace Robot
                 var motor = motorHat.CreateDCMotor(1); 
 
                 // Speed goes from -1 to 1, where -1 is max backward speed, 1 is max forward speed and 0 means stopping the motor
-                motor.Speed = 1; 
+                motor.Speed = 1;
+                Console.ReadLine();
             }
         }
     }
